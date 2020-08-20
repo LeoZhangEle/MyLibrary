40100578cee047acd4b2e77eba93ca565de94654
LeoZhang <Leo.Zhang.ele@gmail.com>
2020-08-20 15:07:02 +0800

    adds author resource

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 
 Route::post('books','BooksController@store');
 Route::patch('/books/{book}','BooksController@update');
+Route::delete('/books/{book}','BooksController@destroy');
+
+//Route::patch('/books/{book}-{slug}','BooksController@update');
+//Route::delete('/books/{book}-{slug}','BooksController@destroy');
+
+Route::post('author','AuthorController@store');
